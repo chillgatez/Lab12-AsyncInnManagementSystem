8490bea6954023ff3f73ad3d5c584928640950de
DJ <epicmrjones@gmail.com>
2023-07-27 09:26:38 -0500

    Wednesday 7/25

---
+++ WebApplication1/Models/Amenity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab12.Models
+{
+    public class Amenity
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+    
+    }
+}
