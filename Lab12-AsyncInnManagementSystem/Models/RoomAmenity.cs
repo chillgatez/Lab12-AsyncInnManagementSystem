486ee9a485b0b42d261484c1eac6dae46d6d5da1
Kelsee Watts <watts.kelsee@gmail.com>
2023-08-02 21:53:52 -0500

    sone with models. added a connection string and context file

---
+++ Lab12-AsyncInnManagementSystem/Models/RoomAmenity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab12_AsyncInnManagementSystem.Models
+{
+    public class RoomAmenity
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public int RoomID { get; set; }
+        [Required]
+        public int AmenityID { get; set; }
+    }
+}
